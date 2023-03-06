99c621b2ca85ce9d97e31404f896ca536d3cc77c
jhandrahan <jessicahandrahan@gmail.com>
2023-03-06 18:37:28 -0400

    Initialize react app, installed axios and routers, Added Form, Header and Homepage Components

---
+++ src/components/Form.js
@@ -0,0 +1,13 @@
+const Form = () => {
+    return(
+        <>
+        <form action="">
+            <label htmlFor="search"></label>
+            <input type="text" placeholder="" />
+            <button>Search</button>
+        </form>
+        </>
+    )
+}
+
+export default Form;
---
+++ src/components/Homepage.js
@@ -0,0 +1,13 @@
+import Header from './Header';
+import Form from './Form';
+
+const Homepage = () => {
+    return(
+        <main>
+            <Header />
+            <Form />
+        </main>
+    )
+}
+
+export default Homepage;
